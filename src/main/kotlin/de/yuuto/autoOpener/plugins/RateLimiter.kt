caff48b82a946653f08cde76e278da4bf9b98e1a
leo <lt77yt@gmail.com>
2025-04-07 23:15:44 +0200

    Add request key based on remote address for rate limiting

--- src/main/kotlin/de/yuuto/autoOpener/plugins/RateLimiter.kt
+++ src/main/kotlin/de/yuuto/autoOpener/plugins/RateLimiter.kt
@@ -2,6 +2,7 @@ package de.yuuto.autoOpener.plugins
 
 import de.yuuto.autoOpener.util.Config
 import io.ktor.server.application.*
+import io.ktor.server.plugins.origin
 import io.ktor.server.plugins.ratelimit.*
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
@@ -10,12 +11,15 @@ fun Application.configureRateLimiter() {
     install(RateLimit) {
         register(RateLimitName("service")) {
             rateLimiter(limit = 1000, refillPeriod = 1.minutes)
+            requestKey { call -> call.request.origin.remoteAddress }
         }
         register(RateLimitName("token")) {
             rateLimiter(limit = Config.getRateLimits().token.limit, refillPeriod = Config.getRateLimits().token.duration.seconds)
+            requestKey { call -> call.request.origin.remoteAddress }
         }
         register(RateLimitName("websocket")) {
             rateLimiter(limit = Config.getRateLimits().websocket.limit, refillPeriod = Config.getRateLimits().websocket.duration.seconds)
+            requestKey { call -> call.request.origin.remoteAddress }
         }
     }
 }
